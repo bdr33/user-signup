23c2582d2589db32515780d057425b27e2bd7734
Brittany Robinson <bdr3300@gmail.com>
2018-04-16 20:38:20 -0500

    errors created

--- main.py
+++ main.py
@@ -8,24 +8,32 @@ form="""
 <html>
     <head>
         <style>
-            .error { color: red; }
+            .error {{ color: red; }}
         </style>
     </head>
     <body>
         <h1>Signup</h1>
-        <form action="/welcome" method="post">
+        <form action="/validate-form" method="post">
             <label>Username:
-                <input id="user_name" name="user_name" type="text"/>
+                <input id="user_name" name="user_name" type="text" value='{user_name}'/>
             </label>
+            <p class="error">{user_name_error}</p>
+
             <label>Password 
-                <input name="password" type="password"/>
+                <input id="password" name="password" type="password" value='{password}'/>
             </label>
+            <p class="error">{password_error}</p>
+
             <label>Verify Password
-                <input name="password_ver" type="password"/>
+                <input id="password_ver" name="password_ver" type="password" value='{password_ver}'/>
             </label>
+            <p class="error">{password_ver_error}</p>
+
             <label>Email (optional):
-                <input name="email" type="text" />
+                <input id="email" name="email" type="text" value='{email}'/>
             </label>
+            <p class="error">{email_error}</p>
+
             <input type="submit" />
         </form>
     </body>
@@ -34,11 +42,33 @@ form="""
 
 @app.route("/")
 def index():
-    return form
+    return form.format(user_name='', user_name_error='', 
+    password='', password_error='', password_ver='', password_ver_error='', email='', email_error='')
 
-@app.route("/welcome", methods=['POST'])
-def welcome():
+@app.route("/validate-form", methods=['POST'])
+def validate_form():
+
     user_name= request.form['user_name']
-    return '<h1>Hello, ' + user_name + '</h1>'
+    password= request.form['password']
+    password_ver= request.form['password_ver']
+    email= request.form['email']
+
+    user_name_error= ''
+    password_error= ''
+    password_ver_error=''
+    email_error=''
+    empty_error=''
+
+    if user_name=="":
+        user_name_error = 'You must enter a username'
+        
+    if password=="":
+        password_error = 'You must enter a password'
+            
+    if password_ver == "":
+        password_ver_error = "Passwords don't match"
+        ****return formatted form
+    else:
+        return '<h1>Hello, ' + user_name + '</h1>'
 
 app.run()
