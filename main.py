195c832d61c98d4db2136d30776d9e7f1b432ac2
Brittany Robinson <bdr3300@gmail.com>
2018-04-17 17:12:17 -0500

    working without templates

--- main.py
+++ main.py
@@ -57,18 +57,43 @@ def validate_form():
     password_error= ''
     password_ver_error=''
     email_error=''
-    empty_error=''
 
     if user_name=="":
         user_name_error = 'You must enter a username'
+    else: 
+        if len(user_name) <3 or len(user_name) >20:
+            user_name_error= 'Your username must be between 3 and 20 characters long'
+        else: 
+            if " " in user_name:
+                user_name_error= 'Your username cannot contain any spaces'
+
         
     if password=="":
         password_error = 'You must enter a password'
+    else: 
+        if len(password) <3 or len(password)>20:
+            password_error= 'Your password must be between 3 and 20 characters long'
+        else: 
+            if " " in password: 
+                password_error= 'Your password cannot contain any spaces'
             
-    if password_ver == "":
+    if password_ver == "" or password_ver != password:
         password_ver_error = "Passwords don't match"
-        ****return formatted form
-    else:
+    
+    if len(email) <3 or len(email)>20 or " " in email: 
+        email_error='Please enter a valid email'
+    else: 
+        if email.count('@') != 1 or email.count('.') !=1:
+            email_error='Please enter a valid email'
+    
+
+    if not user_name_error and not password_error and not password_ver_error and not email_error:
         return '<h1>Hello, ' + user_name + '</h1>'
+
+    else: 
+        return form.format(user_name=user_name, password='', 
+        password_ver='', email=email, user_name_error=user_name_error, password_error=password_error,
+         password_ver_error=password_ver_error, email_error=email_error)
+
 
 app.run()
