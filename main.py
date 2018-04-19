543a5e6090e9865c688472d5f491a66c5fe9fc15
Brittany Robinson <bdr3300@gmail.com>
2018-04-19 16:54:09 -0500

    Passing index template

--- main.py
+++ main.py
@@ -10,48 +10,12 @@ jinja_env= jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), au
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form="""
-<!doctype html>
-<html>
-    <head>
-        <style>
-            .error {{ color: red; }}
-        </style>
-    </head>
-    <body>
-        <h1>Signup</h1>
-        <form action="/validate-form" method="post">
-            <label>Username:
-                <input id="user_name" name="user_name" type="text" value='{user_name}'/>
-            </label>
-            <p class="error">{user_name_error}</p>
-
-            <label>Password 
-                <input id="password" name="password" type="password" value='{password}'/>
-            </label>
-            <p class="error">{password_error}</p>
-
-            <label>Verify Password
-                <input id="password_ver" name="password_ver" type="password" value='{password_ver}'/>
-            </label>
-            <p class="error">{password_ver_error}</p>
-
-            <label>Email (optional):
-                <input id="email" name="email" type="text" value='{email}'/>
-            </label>
-            <p class="error">{email_error}</p>
-
-            <input type="submit" />
-        </form>
-    </body>
-</html>
-"""
 
 @app.route("/")
 def index():
-    return form.format(user_name='', user_name_error='', 
-    password='', password_error='', password_ver='', password_ver_error='', email='', email_error='')
-
+    template = jinja_env.get_template('index_form.html')
+    return template.render()
+    
 @app.route("/validate-form", methods=['POST'])
 def validate_form():
 
